de3b5c9ea1dc3c9161ed51ebf903cd857e84faa3
bc238dev <46238915+bc238dev@users.noreply.github.com>
2022-10-12 09:54:47 -0700

    3 files modified

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from 'preact/hooks';
+import preactLogo from './assets/preact.svg';
+import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,18 +15,18 @@ export function App() {
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
       </div>
-      <h1>Vite + Preact</h1>
+      <h1>O: Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
+          O: Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
       <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
       </p>
     </>
-  )
+  );
 }
